e44a86e95c0ebdab56cfb4736acb9cada14dea5e
pangjian <pangj1990@163.com>
2017-06-04 23:11:21 +0800

    fix

---
+++ src/main/java/com/pj/ServerApplication.java
@@ -0,0 +1,25 @@
+package com.pj;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+/**
+ * Created by pangjian on 6/4/17.
+ */
+@Controller
+@EnableAutoConfiguration
+public class ServerApplication {
+
+    @RequestMapping("/")
+    @ResponseBody
+    String home() {
+        return "Hello World!";
+    }
+
+    public static void main(String[] args) throws Exception {
+        SpringApplication.run(ServerApplication.class, args);
+    }
+}
